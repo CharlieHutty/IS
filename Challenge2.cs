f82a884992b44cb0e7cf4bbaf9cb3676f4e69178
CharlieHutty <46685705+CharlieHutty@users.noreply.github.com>
2019-01-27 16:18:10 +0000

    Add files via upload

---
+++ Challenge2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenege_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            //This is for the two cameras
+            //Using the DateTime variable type so that the user can input the specific times
+            DateTime camera1;
+            DateTime camera2;
+            //This uses the TimeSpan variable type so that it can work out the difference between both times
+            TimeSpan time;
+            double hours;
+            double speed;
+            int distance = 1;
+
+            //Allow the user to input the time the car went past the first camera
+            Console.WriteLine("Please enter the time in the format HH:MM:SS");
+            Console.Write("What was the time of the first camera: ");
+            camera1 = Convert.ToDateTime(Console.ReadLine());
+
+            //Allows the user to input the time the car went past the second camera
+            Console.WriteLine("Please enter the time in the format HH:MM:SS");
+            Console.Write("What was the time of the second camera: ");
+            camera2 = Convert.ToDateTime(Console.ReadLine());
+
+            //Works out the distance between the times
+            //Converts this to hours
+            time = camera2 - camera1;
+            hours = time.TotalHours;
+
+            //Uses the speed equation to work out the speed
+            speed = (distance / hours);
+
+            //Outputs the speed to the user
+            Console.WriteLine(speed + "mph");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Challenge6.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenege_6
+{
+    class Program
+    {
+        static string Conversion;
+        static double NumberToConvert;
+        static double Answer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celcius to Fahrenheit ('CtoF'), Fahrenheit to Celcius ('FtoC')");
+            Console.WriteLine("Pounds to Euro ('GBPtoEuro'), Pounds to Dollars ('EurotoGBP')");
+            Console.WriteLine("Litres to Millitres ('LtoMl'), Millitres to Litres ('MltoL')");
+            Console.Write("What conversion would you like to do: ");
+            Conversion = Console.ReadLine();
+
+            Console.Write("What is the number you'd like to convert: ");
+            NumberToConvert = Convert.ToInt32(Console.ReadLine());
+
+            if (Conversion == "CtoF" || Conversion == "ctof" || Conversion == "Celcius to Fahrenheit")
+            {
+                CtoF();
+            }
+            else if (Conversion == "FtoC" || Conversion == "ftoc" || Conversion == "Fahrenheit to Celcius")
+            {
+                FtoC();
+            }
+            else if (Conversion == "GBPtoEuro" || Conversion == "gbptoeuro" || Conversion == "Pounds to Euro")
+            {
+                GBPtoEuro();
+            }
+            else if (Conversion == "EurotoGBP" || Conversion == "eurotpgbp" || Conversion == "Euro to Pounds")
+            {
+                EurotoGBP();
+            }
+            else if (Conversion == "LitrestoMillimetres" || Conversion == "litrestomillitres" || Conversion == "Litres to Millitres")
+            {
+                LitrestoMillitres();
+            }
+            else if (Conversion == "MillitrestoLitres" || Conversion == "millitrestolitres" || Conversion == "Millitres to Litres")
+            {
+                MillitrestoLitres();
+            }
+
+            Console.ReadKey();
+        }
+        static void CtoF()
+        {
+            //(Celcius * 1.8) + 32 
+            Answer = ((NumberToConvert * 1.8) + 32);
+            Console.WriteLine(Answer + "°F");
+        }
+        static void FtoC()
+        {
+            //(Fahrenheit - 32) * 5/9
+            Answer = (NumberToConvert - 32) * 5 / 9;
+            Console.WriteLine(Answer + "°C");
+        }
+        static void GBPtoEuro()
+        {
+            //Pounds * 1.13
+            Answer = (NumberToConvert * 1.13);
+            Console.WriteLine("€" + Answer);
+        }
+        static void EurotoGBP()
+        {
+            //Euro / 1.13
+            Answer = (NumberToConvert / 1.13);
+            Console.WriteLine("£" + Answer);
+        }
+        static void LitrestoMillitres()
+        {
+            //Litres * 1000
+            Answer = (NumberToConvert * 1000);
+            Console.WriteLine(Answer + "ML");
+        }
+        static void MillitrestoLitres()
+        {
+            //Millitres / 1000 
+            Answer = (NumberToConvert / 1000);
+            Console.WriteLine(Answer + "L");
+        }
+
+    }
+}
